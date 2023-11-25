a913d36541df628b0ad1ccaf3613989e65a8ad57
ocdpment003 <ocdpment003@outlook.com>
2023-11-25 12:11:22 +0800

    feat* 新增静态Static Method

--- JS_CLASS/index.js
+++ JS_CLASS/index.js
@@ -27,3 +27,30 @@ class NewUser {
 const user1 = new NewUser('James')
 
 user1.sayHi()
+
+// Class的静态方法（Static Method)
+class Article {
+  constructor(article) {
+    this.article = article
+  }
+  static compare(a, b) {
+    return a.date < b.date ? -1 : 1
+  }
+}
+// 等同于
+// Article.compare = function (a, b) {
+//   return a.date < b.date ? -1 : 1
+// }
+
+const goods_Article = new Article([
+  {
+    name: '斗罗大陆',
+    date: 2024
+  },
+  {
+    name: '西游记',
+    date: 2023
+  }
+])
+
+console.log(goods_Article.article.sort(Article.compare))
